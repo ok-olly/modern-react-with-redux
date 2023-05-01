982c5150c877a17860a52079a2380df80dd63076
ok-olly <jeongoklee0108@gmail.com>
2023-05-01 17:48:21 +0900

    35. Handling Image Accessibility

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -3,23 +3,14 @@ import AlexaImage from './images/alexa.png';
 import CortanaImage from './images/cortana.png';
 import SiriImage from './images/siri.png';
 
-// 9.7KB 이상인 이미지 파일을 import해오면 외부 파일 그 자체로 처리되고
-// 이하인 파일을 가져오면 base64로 인코딩되어 js파일에 인라인으로 들어간다.
-// 폰트나 소리 파일도 마찬가지.
-console.log(AlexaImage);
-console.log(SiriImage);
-
 function App() {
   return (
     <div>
       <div>Personal Digital Assistants</div>
 
-      <img src={AlexaImage}></img>
-      <img src={SiriImage}></img>
-
-      <ProfileCard title="Alexa" handle="@alexa99" />
-      <ProfileCard title="Cortana" handle="@cortana32" />
-      <ProfileCard title="Siri" handle="@siri01" />
+      <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+      <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />
+      <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
     </div>
   );
 }
