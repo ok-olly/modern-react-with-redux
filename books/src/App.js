56ef1f77fd465bb24ce1bab8a82824eaef7c46f6
ok-olly <jeongoklee0108@gmail.com>
2023-06-09 13:56:45 +0900

    123. fetch a list of records

--- books/src/App.js
+++ books/src/App.js
@@ -6,6 +6,15 @@ import BookList from './components/BookList';
 function App() {
   const [books, setBooks] = useState([]);
 
+  const fetchBooks = async () => {
+    const response = await axios.get('http://localhost:3001/books');
+
+    setBooks(response.data);
+  };
+
+  // DONT DO THIS
+  // fetchBooks();
+
   const editBookById = (id, newTitle) => {
     const updatedBooks = books.map(book => {
       if (book.id === id) {
